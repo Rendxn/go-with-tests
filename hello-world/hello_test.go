83ec70da86f25fed06c9aaabc127dbd7ee753201
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2023-01-14 22:47:38 -0500

    feat: hello world in multiple languages

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+		got := Hello("Santiago", "")
+		want := "Hello, Santiago"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Santiago", "Spanish")
+		want := "Hola, Santiago"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Santiago", "French")
+		want := "Bonjour, Santiago"
 		assertCorrectMessage(t, got, want)
 	})
 
