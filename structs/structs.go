1de9591e1bdbf3f1a180878facf4091b495b7767
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2023-01-20 23:35:17 -0500

    feat: shape interface for decoupling and DRYness

--- structs/structs.go
+++ structs/structs.go
@@ -2,6 +2,10 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -11,14 +15,27 @@ type Circle struct {
 	Radius float64
 }
 
-func (r *Rectangle) Perimeter() float64 {
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
 
-func (r *Rectangle) Area() float64 {
+func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
-func (c *Circle) Area() float64 {
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (c Circle) Area() float64 {
 	return math.Pow(c.Radius, 2) * math.Pi
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -8,28 +8,30 @@ func TestPerimeter(t *testing.T) {
 	want := 40.0
 
 	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+		t.Errorf("got %v want %v", got, want)
 	}
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 364.0},
+	}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	}
+
 }
